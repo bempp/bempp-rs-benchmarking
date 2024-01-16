faa60f91315e418a330565b24ed8d88b9c216ee5
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-01-16 15:43:16 +0000

    layout

--- make_html.py
+++ make_html.py
@@ -9,11 +9,7 @@ with open(os.path.join(root_dir, "data.json")) as f:
 benches = list(data.keys())
 benches.sort()
 
-# print("<h2>All benchmarks</h2>")
 print("<div id='benchall'></div>")
-# for i, b in enumerate(benches):
-#    print(f"<h2>{b}</h2>")
-#    print(f"<div id='bench{i}'></div>")
 
 print("<script type='text/javascript'>")
 for i, b in enumerate(benches):
@@ -24,7 +20,13 @@ for i, b in enumerate(benches):
     print("  mode: 'lines+markers',")
     print(f"  name: \"{b}\"")
     print("};")
-print("Plotly.newPlot('benchall', [" + ", ".join([f"line{i}" for i, _ in enumerate(benches)]) + "]);")
-# for i, _ in enumerate(benches):
-#    print(f"Plotly.newPlot('bench{i}', [line{i}]);")
+
+print("var layout = {")
+print("  showlegend: true,")
+print("  legend: {\"orientation\": \"h\"}")
+print("};")
+
+print("Plotly.newPlot('benchall', "
+      "[" + ", ".join([f"line{i}" for i, _ in enumerate(benches)]) + "], "
+      "layout);")
 print("</script>")
